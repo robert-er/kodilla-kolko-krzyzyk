5342c73e2c79741ea0db0ce66801db4a71a3f2bb
robert-er <robert.roza.pl@gmail.com>
2020-07-16 16:57:44 +0200

    start with AI

--- src/main/java/com/kodilla/tictactoe/Computer.java
+++ src/main/java/com/kodilla/tictactoe/Computer.java
@@ -1,25 +1,42 @@
 package com.kodilla.tictactoe;
 
-import java.util.concurrent.TimeUnit;
-
 public class Computer {
 
     private static GameState gameState = GameState.getGameState();
     private static Content content = gameState.getContent();
     private static Tile[][] board = content.getBoard().getTiles();
+    private static int[][] prediction = new int[3][3];
 
     public static void playComputer() throws InterruptedException {
         System.out.println("Computer.playComputer");
-        System.out.println("Tile.isComputerTurn()=" + Tile.isComputerTurn()
-                + " Tile.isTurnX()=" + Tile.isTurnX());
         Tile.setComputerTurn(true);
         if (Tile.isPlayable()) {
             for (Tile[] tile : board) {
                 for (Tile tile1 : tile) {
                     if (!tile1.isSelected()) {
+                        System.out.println("tile value " + tile1.getValue());
                         tile1.playComputer();
                         break;
                     }
+                }
+            }
+
+//            for (Tile[] tile2 : board) {
+//                for (Tile tile3 : tile2) {
+//                    System.out.println(" all tiles vlues: " + tile3.getValue());
+//                }
+//            }
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+
+                    if (board[j][i].getValue() == Game.type.X) {
+                        prediction[j][i] = -1;
+                    }
+                    if (board[j][i].getValue() == Game.type.O) {
+                        prediction[j][i] = 1;
+                    }
+                    System.out.println(" all tiles vlues: " + prediction[j][i]);
                 }
             }
         }
